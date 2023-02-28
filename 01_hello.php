59853230f7e48822debbcfbdc00ae91f60cbe859
sadikNinja15 <119508074+sadikNinja15@users.noreply.github.com>
2023-02-28 16:03:50 +0530

    Add files via upload

---
+++ 01_hello.php
@@ -0,0 +1,7 @@
+<?php  //-   Opening tag of a block
+
+echo "hello" // echo is a command to print to the screen
+
+;   //-  terminates the command
+
+?>   //  -   closing tag of a block
---
+++ 05_string.php
@@ -0,0 +1,25 @@
+<?php 
+
+$name = "sadik is a good boy";
+echo $name;
+echo "<br>";
+
+echo "The length of " . "my string is " . strlen($name);
+echo "<br>";
+echo str_word_count($name);
+echo "<br>";
+echo strrev($name);
+echo "<br>";
+echo strpos($name, "sadik");
+echo "<br>";
+echo str_replace("sadik", "Rohan", $name);
+echo "<br>";
+echo str_repeat($name, 4);
+echo "<br>";
+echo "<pre>";
+echo rtrim("    this is a good boy     ");
+echo "<br>";
+echo ltrim("    this is a good boy     ");
+echo "</pre>";
+
+?>
